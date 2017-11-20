87ee127b74ea17cda53f68f541e6448b64d61b80
Fly J Gore <jennifer@theothergore.com>
2017-11-20 19:33:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(greeting){
+  console.log(`${greeting.toUpperCase()}`)
+}
+function logWhisper(greeting){
+  console.log(`${greeting.toLowerCase()}`)
+}
+function sayHiToGrandma(string) {
+  if (string === whisper(string))
+  return "I can't hear you!";
+}
+// else if (string === shout(string)) {
+//   return "YES INDEED!";
+// }
+// // else (string === "I love you, Grandma.") {
+// //   return "I love you, too.";
+// // }
